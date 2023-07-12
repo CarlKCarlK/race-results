5733f931c1d458629014a74eb531c1327394b21a
Carl Kadie <carlk@msn.com>
2023-07-12 11:58:48 -0700

    try to release again

---
+++ docs/matcher/v0.1.2/pkg/web1_bg.wasm.d.ts
@@ -0,0 +1,8 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function member_match(a: number, b: number, c: number, d: number, e: number): number;
+export function sample_members(): number;
+export function sample_results(): number;
+export function __wbindgen_malloc(a: number, b: number): number;
+export function __wbindgen_realloc(a: number, b: number, c: number, d: number): number;
